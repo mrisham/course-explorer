3b5e9e9bab956c0261983e4c97c6191ccfef7215
Mridul Shukla <75447397+mrisham@users.noreply.github.com>
2024-01-20 00:17:33 +0530

    added dashboard

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,6 +1,5 @@
 import Header from "./Header";
 import { Grid } from "@mui/material";
-import SearchBar from "./SearchBar";
 import Body from "./Body";
 
 const Layout = () => {
@@ -11,11 +10,6 @@ const Layout = () => {
           <Header />
         </item>
       </Grid>
-      {/* <Grid marginTop={3} marginLeft={2} xs={100}>
-        <item>
-          <SearchBar width="70%" />
-        </item>
-      </Grid> */}
       <Grid marginTop={1} marginLeft={0} xs={100}>
         <item>
           <Body />
