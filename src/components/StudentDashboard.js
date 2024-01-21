8073fc36023d7bfd82957cc1655b9b166ffbad7f
Mridul Shukla <75447397+mrisham@users.noreply.github.com>
2024-01-21 23:58:36 +0530

    student dashboard

--- src/components/StudentDashboard.js
+++ src/components/StudentDashboard.js
@@ -1,19 +1,72 @@
-// StudentDashboard.js
-import React from "react";
-import { useSelector } from "react-redux";
+import React, { useEffect, useState } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import Container from "@mui/material/Container";
-import CardInfo from "./CardInfo";
+import LinearProgress from "@mui/material/LinearProgress";
+import Button from "@mui/material/Button";
+import { setCourse } from "../utils/courseSlice";
 
 const StudentDashboard = () => {
+  const dispatch = useDispatch();
   const enrolledCourses = useSelector((state) =>
-    state.course.data.filter((course) => course.enrolled === "true")
+    state.course.data.filter((course) => course.enrolled)
   );
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch("/api/notes");
+        if (!response.ok) {
+          throw new Error(`HTTP error! Status: ${response.status}`);
+        }
+
+        const data = await response.json();
+        const notesArray = data.notes;
+
+        dispatch(setCourse(notesArray));
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      }
+    };
+
+    fetchData();
+  }, [dispatch]);
+
+  const [completedCourses, setCompletedCourses] = useState({});
+
+  const handleMarkAsCompleted = (courseId) => {
+    setCompletedCourses((prevCompletedCourses) => ({
+      ...prevCompletedCourses,
+      [courseId]: !prevCompletedCourses[courseId],
+    }));
+  };
 
   return (
     <Container>
       <h2>Your Enrolled Courses</h2>
       {enrolledCourses.map((course) => (
-        <CardInfo key={course.id} course={course} />
+        <div key={course.id}>
+          <h3>{course.name}</h3>
+          <p>Instructor: {course.instructor}</p>
+          <img
+            src={course.thumbnail}
+            alt={course.name}
+            style={{ maxWidth: "100px" }}
+          />
+          <p>Due Date: {course.dueDate}</p>
+          <LinearProgress
+            variant="determinate"
+            value={
+              completedCourses[course.id] ? 100 : parseInt(course.progress, 10)
+            }
+          />
+          <Button
+            variant={completedCourses[course.id] ? "contained" : "outlined"}
+            onClick={() => handleMarkAsCompleted(course.id)}
+          >
+            {completedCourses[course.id] ? "Completed" : "Mark as Completed"}
+          </Button>
+          <hr />
+        </div>
       ))}
     </Container>
   );
